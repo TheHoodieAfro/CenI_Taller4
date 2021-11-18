305d7933558aaff4036b2a2c690329c45f736049
Cristian Sanchez <crisey937@gmail.com>
2021-11-17 23:24:05 -0500

    Completed controllers except for edits, deletes and posible references

--- src/main/java/com/taller/controller/AdminController.java
+++ src/main/java/com/taller/controller/AdminController.java
@@ -1,64 +1,189 @@
 package com.taller.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.BindingResult;
 
 import com.taller.model.Product;
+import com.taller.model.Productcategory;
+import com.taller.model.Productsubcategory;
+import com.taller.model.Productvendor;
+import com.taller.model.Unitmeasure;
 import com.taller.model.Vendor;
 import com.taller.service.implementations.ProductServiceImp;
+import com.taller.service.implementations.ProductcategoryServiceImp;
+import com.taller.service.implementations.ProductsubcategoryServiceImp;
+import com.taller.service.implementations.ProductvendorServiceImp;
+import com.taller.service.implementations.UnitmeasureServiceImp;
 import com.taller.service.implementations.VendorServiceImp;
 
 @Controller
 public class AdminController {
 	
+	//------------------------------------------------------- Services -------------------------------------------------------
 	ProductServiceImp ps;
+	ProductcategoryServiceImp pcs;
+	ProductsubcategoryServiceImp pscs;
+	UnitmeasureServiceImp ums;
 	
+	ProductvendorServiceImp pvs;
 	VendorServiceImp vs;
 	
-	public AdminController(ProductServiceImp ps, VendorServiceImp vs) {
+	//------------------------------------------------------- Constructor -------------------------------------------------------
+	@Autowired
+	public AdminController(ProductServiceImp ps, ProductvendorServiceImp pvs, VendorServiceImp vs, UnitmeasureServiceImp ums, ProductcategoryServiceImp pcs, ProductsubcategoryServiceImp pscs) {
 		this.ps = ps;
+		this.pvs = pvs;
 		this.vs = vs;
+		this.ums = ums;
+		this.pcs = pcs;
+		this.pscs = pscs;
 	}
 	
-	@GetMapping("/products")
+	//------------------------------------------------------- Index -------------------------------------------------------
+	@GetMapping("/product")
     public String products(Model model) {
 		model.addAttribute("products", ps.findAll());
         return "admin/products";
     }
 	
-	@GetMapping("/addProduct")
+	@GetMapping("/productvendor")
+    public String Productvendors(Model model) {
+		model.addAttribute("productvendors", pvs.findAll());
+        return "admin/productvendors";
+    }
+	
+	//------------------------------------------------------- Save -------------------------------------------------------
+	@GetMapping("/product/add")
 	public String addProduct(Model model) {
 		model.addAttribute("product", new Product());
 		return "admin/addProduct";
 	}
 	
-	@PostMapping("/addProduct")
-	public String saveProduct(Product product, Model model, @RequestParam(value = "action", required = true) String action) {
-		if (!action.equals("Cancel"))
-			ps.save(product);
-		return "redirect:/products";
+	@PostMapping("/product/add")
+	public String saveProduct(@Validated Product product, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("product", new Product());
+			return "admin/addProduct";
+		}
+		
+		if (action.equals("Cancel")) {
+			return "redirect:/product";
+		}
+		ps.save(product);
+		return "redirect:/product";
 	}
 	
-	@GetMapping("/vendors")
-    public String vendors(Model model) {
-		model.addAttribute("vendors", vs.findAll());
-        return "admin/vendors";
-    }
+	@GetMapping("/productcategory/add")
+	public String addProductcategory(Model model) {
+		model.addAttribute("productcategory", new Productcategory());
+		return "admin/addProductcategory";
+	}
 	
-	@GetMapping("/addVendors")
+	@PostMapping("/productcategory/add")
+	public String saveProductcategory(@Validated Productcategory productcategory, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("productcategory", new Productcategory());
+			return "admin/addProductcategory";
+		}
+		
+		if (action.equals("Cancel")) {
+			return "redirect:/productcategory/add";
+		}
+		pcs.save(productcategory);
+		return "redirect:/productcategory/add";
+	}
+	
+	@GetMapping("/productsubcategory/add")
+	public String addProductsubcategory(Model model) {
+		model.addAttribute("productsubcategory", new Productsubcategory());
+		model.addAttribute("productcategories", pcs.findAll());
+		return "admin/addProductsubcategory";
+	}
+	
+	@PostMapping("/productsubcategory/add")
+	public String saveProductsubcategory(@Validated Productsubcategory productsubcategory, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("productsubcategory", new Productsubcategory());
+			model.addAttribute("productcategories", pcs.findAll());
+			return "admin/addProductsubcategory";
+		}
+		
+		if (action.equals("Cancel")) {
+			return "redirect:/productsubcategory/add";
+		}
+		pscs.save(productsubcategory);
+		return "redirect:/productsubcategory/add";
+	}
+	
+	@GetMapping("/unitmeasure/add")
+	public String addUnitmeasure(Model model) {
+		model.addAttribute("unitmeasure", new Unitmeasure());
+		return "admin/addUnitmeasure";
+	}
+	
+	@PostMapping("/unitmeasure/add")
+	public String saveUnitmeasure(@Validated Unitmeasure unitmeasure, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("unitmeasure", new Unitmeasure());
+			return "admin/addUnitmeasure";
+		}
+		
+		if (action.equals("Cancel")) {
+			return "redirect:/unitmeasure/add";
+		}
+		ums.save(unitmeasure);
+		return "redirect:/unitmeasure/add";
+	}
+	
+	@GetMapping("/productvendor/add")
+	public String addProductvendor(Model model) {
+		model.addAttribute("productvendor", new Productvendor());
+		return "admin/addProductvendor";
+	}
+	
+	@PostMapping("/productvendor/add")
+	public String saveProductvendor(@Validated Productvendor productvendor, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("productvendor", new Productvendor());
+			return "admin/addProductvendor";
+		}
+		
+		if (action.equals("Cancel")) {
+			return "redirect:/productvendor";
+		}	
+		pvs.save(productvendor);
+		return "redirect:/productvendor";
+	}
+	
+	@GetMapping("/vendor/add")
 	public String addVendor(Model model) {
 		model.addAttribute("vendor", new Vendor());
 		return "admin/addVendor";
 	}
 	
-	@PostMapping("/addVendor")
-	public String saveVendor(Vendor vendor, Model model, @RequestParam(value = "action", required = true) String action) {
-		if (!action.equals("Cancel"))
-			vs.save(vendor);
-		return "redirect:/vendors";
+	@PostMapping("/vendor/add")
+	public String saveVendor(@Validated Vendor vendor, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("vendor", new Vendor());
+			return "admin/addVendor";
+		}
+		
+		if (action.equals("Cancel")) {
+			return "redirect:/productvendor";
+		}	
+		vs.save(vendor);
+		return "redirect:/productvendor";
 	}
+	
+	//------------------------------------------------------- Edit -------------------------------------------------------
+	
+	
+	//------------------------------------------------------- Extra methods -------------------------------------------------------
 	
 }
--- src/main/java/com/taller/controller/OperatorController.java
+++ src/main/java/com/taller/controller/OperatorController.java
@@ -1,7 +1,10 @@
 package com.taller.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,51 +21,75 @@ import com.taller.service.implementations.VendorServiceImp;
 @Controller
 public class OperatorController {
 	
+	//------------------------------------------------------- Services -------------------------------------------------------
 	DocumentServiceImp ds;
 	
 	TransactionhistoryServiceImp ths;
 	
+	//------------------------------------------------------- Constructor -------------------------------------------------------
+	@Autowired
 	public OperatorController(DocumentServiceImp ds, TransactionhistoryServiceImp ths) {
 		this.ds = ds;
 		this.ths = ths;
 	}
 	
-	@GetMapping("/documents")
+	//------------------------------------------------------- Index -------------------------------------------------------
+	@GetMapping("/document")
     public String documents(Model model) {
 		model.addAttribute("documents", ds.findAll());
         return "operator/documents";
     }
 	
-	@GetMapping("/addDocument")
+	@GetMapping("/transactionhistory")
+    public String transactionhistories(Model model) {
+		model.addAttribute("transactionhistories", ths.findAll());
+        return "operator/transactionhistories";
+    }
+	
+	//------------------------------------------------------- Save -------------------------------------------------------
+	@GetMapping("/document/add")
 	public String addDocument(Model model) {
 		model.addAttribute("document", new Document());
 		return "operator/addDocument";
 	}
 	
-	@PostMapping("/addDocument")
-	public String saveDocument(Document document, Model model, @RequestParam(value = "action", required = true) String action) {
-		if (!action.equals("Cancel"))
-			ds.save(document);
-		return "redirect:/documents";
+	@PostMapping("/document/add")
+	public String saveDocument(@Validated Document document, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("document", new Document());
+			return "operator/addDocument";
+		}
+		
+		if (!action.equals("Cancel")) {
+			return "redirect:/document";
+		}
+		ds.save(document);
+		return "redirect:/document";
 	}
 	
-	@GetMapping("/transactionHistory")
-    public String transactionHistory(Model model) {
-		model.addAttribute("transactionHistory", ths.findAll());
-        return "operator/transactionHistory";
-    }
-	
-	@GetMapping("/addTransactionHistory")
+	@GetMapping("/transactionhistory/add")
 	public String addTransactionHistory(Model model) {
-		model.addAttribute("transactionHistory", new Transactionhistory());
-		return "operator/addTransactionHistory";
+		model.addAttribute("transactionhistory", new Transactionhistory());
+		return "operator/addTransactionhistory";
 	}
 	
-	@PostMapping("/addTransactionHistory")
-	public String saveTransactionHistory(Transactionhistory transactionHistory, Model model, @RequestParam(value = "action", required = true) String action) {
-		if (!action.equals("Cancel"))
-			ths.save(transactionHistory);
-		return "redirect:/transactionHistory";
+	@PostMapping("/transactionhistory/add")
+	public String saveTransactionHistory(@Validated Transactionhistory transactionhistory, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("transactionhistory", new Transactionhistory());
+			return "operator/addTransactionhistory";
+		}
+		
+		if (!action.equals("Cancel")) {
+			return "redirect:/transactionhistory";
+		}
+		ths.save(transactionhistory);
+		return "redirect:/transactionhistory";
 	}
 	
+	//------------------------------------------------------- Edit -------------------------------------------------------
+	
+	
+	//------------------------------------------------------- Extra methods -------------------------------------------------------
+		
 }
